d98cde4fc70d04242b2cc3dc92e7b8bfde81fc1e
Ibrahim <ibrahim.benrayana@esprit.tn>
2022-12-13 21:03:04 +0100

    Stock pushed

--- src/main/java/tn/test/spring/Entity/Stock.java
+++ src/main/java/tn/test/spring/Entity/Stock.java
@@ -1,31 +1,32 @@
 package tn.test.spring.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
+
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+
+
 public class Stock {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idStock;
+    private int idStock;
 
-    private int qte;
+    private int qte ;
 
-    private int QteMin;
+    private int qteMin ;
 
-    private String libelleStock;
+    private String libelleStock ;
 
-    @OneToMany(mappedBy = "stock")
     @JsonIgnore
+    @OneToMany(mappedBy = "stock")
     private Set<Produit> produits;
+
+
 }
--- src/main/java/tn/test/spring/Repository/StockRepository.java
+++ src/main/java/tn/test/spring/Repository/StockRepository.java
@@ -3,5 +3,9 @@ package tn.test.spring.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tn.test.spring.Entity.Stock;
 
-public interface StockRepository extends JpaRepository<Stock, Long> {
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+
+
 }
+
